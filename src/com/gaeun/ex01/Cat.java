3ab3b36bba98cad5c588a9eec20439f172c8a659
가은 <rkdms8228@naver.com>
2022-05-17 18:19:42 +0900

    cat

---
+++ src/com/gaeun/ex01/Cat.java
@@ -0,0 +1,14 @@
+package com.gaeun.ex01;
+
+public class Cat {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+
+	}
+
+}
